9d87a144f59c5066be7db5ce9801813941a6f547
ishantmehta01 <ishant@harness.io>
2020-08-31 16:51:41 +0530

    Cleanup create-react-app

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAAalXbBg_n-aQg9UNq9gwJ7a7ZbSMlzbo",
+  authDomain: "whatsapp-clone-b96af.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-b96af.firebaseio.com",
+  projectId: "whatsapp-clone-b96af",
+  storageBucket: "whatsapp-clone-b96af.appspot.com",
+  messagingSenderId: "549115322440",
+  appId: "1:549115322440:web:efe4af4647f3e9458ff87d",
+  measurementId: "G-3RLTXGNLQH"
+};
